6b7e3aef81d6a11865131402880277bbbddc39e0
MamaniErick <mamanierick64@gmail.com>
2025-05-08 01:48:07 -0300

    Desarrollo componente[InputTarea.jsx][Mamani Erick]

--- my-app/src/components/InputTarea.jsx
+++ my-app/src/components/InputTarea.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const InputNombre = ()=>{
 
+    const[tarea, setTarea] = useState("");
+
+    const manejarTarea = () => {
+        agregar(tarea);
+        setTarea('');
+    }
+
     return (
         <div>
-
+            <input type="text" placeholder=" Nueva Tarea..."value={tarea} onChange={(e)=>setTarea(e.target.value)}/>
+            <button onClick={manejarTarea}>Agregar</button>
         </div>
     )
 }
