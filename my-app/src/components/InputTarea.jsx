4fc082e814890bd50bb3ca501938d0e8d0afb7af
lucasortiz512 <lucasortiz.lmo@gmail.com>
2025-05-09 23:01:05 -0300

    parteEjercicioAdicional

---
+++ my-app/src/components/InputTarea.jsx
@@ -0,0 +1,20 @@
+import React,{useState} from 'react'
+
+const InputNombre = ({agregar})=>{
+
+    const[tarea, setTarea] = useState("");
+
+    const manejarTarea = () => {
+        agregar(tarea);
+        setTarea('');
+    }
+
+    return (
+        <div>
+            <input type="text" placeholder=" Nueva Tarea..."value={tarea} onChange={(e)=>setTarea(e.target.value)}/>
+            <button onClick={manejarTarea}>Agregar</button>
+        </div>
+    )
+}
+
+export default InputNombre
